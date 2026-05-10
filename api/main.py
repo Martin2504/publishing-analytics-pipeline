0a0ee9e82e4d8c8acd2b0398ba2bf5802e6434a7
Martin Petrov <Martin250401@gmail.com>
2026-05-10 14:41:12 +0100

    Day 10: Added FastAPI detabase dependency injection

--- api/main.py
+++ api/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
 from typing import List
 
 from db.database import SessionLocal
 from db.models import Book
 from api.schemas import BookResponse
+
 """
 API Module
 
@@ -14,34 +16,29 @@ and serve validated data from the pipeline.
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 def root():
     return {"message": "Publishing Analytics API is running"}
 
 @app.get("/books", response_model=List[BookResponse])
-def get_books():
-    session = SessionLocal()
-
-    try:
-        books = session.query(Book).all()
-        return books
-    
-    finally:
-        session.close()
+def get_books(db: Session = Depends(get_db)):
+    return db.query(Book).all()
 
 
 @app.get("/books/{book_id}", response_model=BookResponse)
-def get_book(book_id: int):
-    session = SessionLocal()
-
-    try:
-        book = session.query(Book).filter(Book.id == book_id).first()
-
-        if not book:
-            raise HTTPException(status_code=404, detail="Book not found")
-        
-        return book
+def get_book(book_id: int, db: Session = Depends(get_db)):
+    book = db.query(Book).filter(Book.id == book_id).first()
     
-    finally:
-        session.close()
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    
+    return book
+
 
