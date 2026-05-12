4a5684d801f838a5226214710757e95eef26c2a0
Martin Petrov <Martin250401@gmail.com>
2026-05-12 12:09:45 +0100

    Day 13: Added enviroment-based database configuration

--- db/database.py
+++ db/database.py
@@ -1,3 +1,5 @@
+import os 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db.models import Base
@@ -9,8 +11,10 @@ Sets up the database connection and session management using SQLAlchemy.
 Responsible for creating the engine, session factory, and initializing tables.
 """
 
+load_dotenv()
+
 # SQLite database (file will be created automatically)
-DATABASE_URL = "sqlite:///./books.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./books.db")
 
 engine = create_engine(DATABASE_URL, echo = False)
 
