6225404b98b092d26c339537e8fa8c77321dbc4e
Martin Petrov <Martin250401@gmail.com>
2026-05-15 12:11:26 +0100

    Expanded validation tests to cover all business rules

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -31,6 +31,83 @@ def test_validate_schema_fails_when_column_missing():
 
     assert result is None
 
+def test_validate_business_rules_passes_with_valid_data():
+    df = pd.DataFrame({
+        "id": [1],
+        "title": ["Book A"],
+        "author": ["Author A"],
+        "sales": [100.0]
+    })
+
+    result = validate_business_rules(df)
+
+    assert result is not None
+
+
+def test_validate_business_rules_fails_when_id_not_numeric():
+    df = pd.DataFrame({
+        "id": ["abc"],
+        "title": ["Book A"],
+        "author": ["Author A"],
+        "sales": [100.0]
+    })
+
+    result = validate_business_rules(df)
+
+    assert result is None
+
+
+def test_validate_business_rules_fails_when_id_missing():
+    df = pd.DataFrame({
+        "id": [None],
+        "title": ["Book A"],
+        "author": ["Author A"],
+        "sales": [100.0]
+    })
+
+    result = validate_business_rules(df)
+
+    assert result is None
+
+
+def test_validate_business_rules_fails_when_title_empty():
+    df = pd.DataFrame({
+        "id": [1],
+        "title": ["   "],
+        "author": ["Author A"],
+        "sales": [100.0]
+    })
+
+    result = validate_business_rules(df)
+
+    assert result is None
+
+
+def test_validate_business_rules_fails_when_author_empty():
+    df = pd.DataFrame({
+        "id": [1],
+        "title": ["Book A"],
+        "author": ["   "],
+        "sales": [100.0]
+    })
+
+    result = validate_business_rules(df)
+
+    assert result is None
+
+
+def test_validate_business_rules_fails_when_sales_not_numeric():
+    df = pd.DataFrame({
+        "id": [1],
+        "title": ["Book A"],
+        "author": ["Author A"],
+        "sales": ["abc"]
+    })
+
+    result = validate_business_rules(df)
+
+    assert result is None
+
 def test_validate_business_rules_fails_on_negative_sales():
     df = pd.DataFrame({
         "id": [1],
